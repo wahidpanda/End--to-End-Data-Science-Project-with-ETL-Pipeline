5efe0944292040a86fd50a514ade63ea0c6ecb0c
wahidpanda <islamoahidul12@gmail.com>
2025-03-27 16:41:07 +0600

    Fixed untracked files

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 # STAGE_NAME = "Data Ingestion stage"
 # try:
 #    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    obj = DataIngestionTrainingPipeline()
-#    obj.initiate_data_ingestion()
+#    data_ingestion = DataIngestionTrainingPipeline()
+#    data_ingestion.initiate_data_ingestion()
 #    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 # except Exception as e:
 #         logger.exception(e)
 #         raise e
 
 
-STAGE_NAME = "Data Validation stage"
+# STAGE_NAME = "Data Validation stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = DataValidationTrainingPipeline()
+#    data_ingestion.initiate_data_validation()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+STAGE_NAME = "Data Transformation stage"
 try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataValidationTrainingPipeline()
-        obj.initiate_data_validation()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
+
--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -2,6 +2,9 @@ from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 from src.datascience.entity.config_entity import (DataIngestionConfig)
 from src.datascience.entity.config_entity import (DataValidationConfig)
+from src.datascience.entity.config_entity import (DataTransformationConfig)
+
+
 class ConfigurationManager:
     def __init__(self,
                  config_filepath=CONFIG_FILE_PATH,
@@ -41,3 +44,30 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
