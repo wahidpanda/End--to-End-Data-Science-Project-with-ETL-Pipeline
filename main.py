5bf7486cffe3b1020687f9653e74b64c9197f71e
wahidpanda <islamoahidul12@gmail.com>
2025-03-27 19:18:22 +0600

    model updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 # STAGE_NAME = "Data Ingestion stage"
 # try:
@@ -24,13 +25,24 @@ from src.datascience.pipeline.data_transformation_pipeline import DataTransforma
 #         logger.exception(e)
 #         raise e
 
-STAGE_NAME = "Data Transformation stage"
+# STAGE_NAME = "Data Transformation stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = DataTransformationTrainingPipeline()
+#    data_ingestion.initiate_data_transformation()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+STAGE_NAME = "Model Trainer stage"
+
 try:
+      
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataTransformationTrainingPipeline()
-   data_ingestion.initiate_data_transformation()
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.initiate_model_training()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
-
